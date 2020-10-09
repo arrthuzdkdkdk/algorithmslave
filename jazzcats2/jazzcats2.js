e97327b3f945fc876a66b789a597cc5831dfdb29
luka jerkovic <68723153+arrthuzdkdkdk@users.noreply.github.com>
2020-10-09 20:43:13 +1100

    Add files via upload

--- jazzcats2/jazzcats2.js
+++ jazzcats2/jazzcats2.js
@@ -1,5 +1,6 @@
 var myfont,v;
-var library,songs,song1,song2,song3;
+var library,songs,song1,song2,song3,song4,song5,song6,song7;
+var librarycol,colour,col1,col2,col3;
 let menu = 0;
 var xlip,ylip,xrect,yrect;
 
@@ -8,7 +9,14 @@ function preload() {
   song1 = loadSound('assets/iGetAKickOutOfYou.mp3'); //song file
   song2 = loadSound('assets/naima.mp3');
   song3 = loadSound('assets/loveThemeFromSpartacus.mp3');
+  song4 = loadSound('assets/upperFourHundred.mp3'); //song file
+  song5 = loadSound('assets/theGirlFromIpanema.mp3');
+  song6 = loadSound('assets/imConfessin.mp3');
+  song7 = loadSound('assets/affirmation.mp3');
+  
   myfont = loadFont('assets/constan.ttf'); //font file
+  
+  
 }
 
 
@@ -20,11 +28,27 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  
+  col1 = color(242,133,0);
+  col2 = color(254,216,177);
+  col3 = color(204,204,255);
+  col4 = color(190,190,190);
+  col5 = color(254,216,177);
+  col6 = color(76,187,23);
+  col7 = color(204,204,255);
+  
   textFont(myfont);
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
-  library = [song1,song2,song3]; //dictates "library" as selection of all songs
-  songs = random(library); //picks a random song from library
+  
+  
+  library = [song1,song2,song3,song4,song5,song6,song7]; //dictates "library" as selection of all songs
+  librarycol = [col1,col2,col3,col4,col5,col6,col7];
+  which = random(0,8);
+  colour = librarycol[int(which)];
+  songs = library[int(which)];
+  
+  
   amp = new p5.Amplitude(); //amp is taking the amplitude of the song
   frameRate(1000);
 }
@@ -56,7 +80,7 @@ function draw() {
   if (menu == 1) {
     
     push();
-      fill(0,0,random(0,255));
+      fill(colour);
       noStroke();
       xlip=int(random(-width, width));
       ylip=int(random(-height, height));
@@ -68,7 +92,7 @@ function draw() {
       var volume = amp.getLevel();
       var v = volume*t*100;
       noFill();
-      stroke(255);
+      stroke(colour);
       ellipse(sin(v / 50) * (v / 5), cos(v / 50) * v / 5, sin(v / 30) * 100, 100);
     pop();
    
@@ -89,14 +113,16 @@ function mousePressed() {
   if (songs.isPlaying()) {
     menu = 0;
     songs.stop();
-  } else {
+  } 
+  else {
     menu = 1;
     background(0);
     loop();
-    songs = random(library);
+    which = random(0,8);
+    colour = librarycol[int(which)];
+    songs = library[int(which)];
     amp.setInput(songs);
-    songs.play();
-    
+    songs.play();  
   }
 }
 
