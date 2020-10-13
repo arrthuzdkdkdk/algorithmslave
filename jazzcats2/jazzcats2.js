7476c20ed018213e9835c6592752be60f2476e05
luka jerkovic <68723153+arrthuzdkdkdk@users.noreply.github.com>
2020-10-13 17:15:04 +1100

    Update jazzcats2.js

--- jazzcats2/jazzcats2.js
+++ jazzcats2/jazzcats2.js
@@ -12,7 +12,7 @@ function preload() {
   song1 = loadSound('assets/iGetAKickOutOfYou.mp3'); //song file
   song2 = loadSound('assets/naima.mp3');
   song3 = loadSound('assets/loveThemeFromSpartacus.mp3');
-  song4 = loadSound('assets/upperFourHundred.mp3'); //song file
+  song4 = loadSound('assets/upperFourHundred.mp3'); 
   song5 = loadSound('assets/theGirlFromIpanema.mp3');
   song6 = loadSound('assets/imConfessin.mp3');
   song7 = loadSound('assets/affirmation.mp3');
@@ -32,6 +32,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   
+  //colour library
   col1 = color(242,133,0);
   col2 = color(254,216,177);
   col3 = color(204,204,255);
@@ -43,7 +44,8 @@ function setup() {
   textFont(myfont);
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
-  
+   
+  //arrays for colour and song and lining them up to be the same
   library = [song1,song2,song3,song4,song5,song6,song7]; //dictates "library" as selection of all songs
   librarycol = [col1,col2,col3,col4,col5,col6,col7];
   which = random(0,8);
@@ -63,11 +65,12 @@ function setup() {
 
 function draw() {
   
+  //landing screen
   if (menu == 0) {
     background(0);
     fill(255);
     
-    
+    //rotating text title
     push();
       time = millis();
       textSize(75);
@@ -77,6 +80,7 @@ function draw() {
       text('jazzcats', 0, 0);
     pop();
     
+    //all other text
     textSize(32);
     text('a jazz influenced generative artwork',0,-150);
     textSize(24);
@@ -90,7 +94,9 @@ function draw() {
     
   }
   
+  //artwork generation
   if (menu == 1) {
+    //random ellipses being drawn in background randomly
     push();
       fill(colour);
       noStroke();
@@ -99,6 +105,7 @@ function draw() {
       ellipse(xlip,ylip,40,40);
     pop();
   
+    //the centre ellipse outlines that spin, also fluctuate based on volume
     push(); 
       var t = frameCount%500;
       var volume = amp.getLevel();
@@ -108,6 +115,7 @@ function draw() {
       ellipse(sin(v / 50) * (v / 5), cos(v / 50) * v / 5, sin(v / 30) * 100, 100);
     pop();
    
+   //user input, rotates around the center but mouse position alters this. also fluctuates size based on volume 
    if(mouseIsPressed){
     push();
     fill(0,0,0);
@@ -130,6 +138,7 @@ function draw() {
 
 
 function keyPressed() {
+  //start the sketch and also pick a random song
   if (menu == 0 && key === 't') {
     background(0);
     loop();
@@ -139,18 +148,21 @@ function keyPressed() {
     amp.setInput(songs);
     songs.play();
     menu = 1;
+    //reset only if sketch is playing
   }else if(menu == 1 && key === 'r'){
     songs.stop();
     menu = 0;
+    //next song only if sketch is playing. dictates new random values as well
   }else if(menu == 1 && key === 'n'){
     songs.stop();
     which = random(0,8);
     colour = librarycol[int(which)];
     songs = library[int(which)];
     amp.setInput(songs);
-    time = frameCount/100;
-    songs.play(0,1,1,int(time));
+    time = frameCount/100; //converts frame count to seconds
+    songs.play(0,1,1,int(time)); //next song will continue to play from the duration where old song stopped
     menu = 1;
+    //saves sketch only if it is playing and resets back to landing screen
   }else if(menu === 1 && key === 's'){
     saveCanvas('myCanvas','png');
     songs.stop();
