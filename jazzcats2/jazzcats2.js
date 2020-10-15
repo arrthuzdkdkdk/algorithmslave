f5abbe2b855802f3afc7ffd06292a3dbd272378b
luka jerkovic <68723153+arrthuzdkdkdk@users.noreply.github.com>
2020-10-15 18:19:44 +1100

    Add files via upload

--- jazzcats2/jazzcats2.js
+++ jazzcats2/jazzcats2.js
@@ -1,22 +1,26 @@
 var myfont,v;
-var library,songs,song1,song2,song3,song4,song5,song6,song7;
+var library,songs,song1,song2,song3,song4,song5,song6,song7,song8,song9,song10;
 var prevSong;
-var librarycol,colour,col1,col2,col3;
+var librarycol,colour,col1,col2,col3,col4,col5,col6,col7,col8,col9,col10;
 var menu = 0;
 var xlip,ylip,xrect,yrect;
 var time;
 var r = 0;
-
+var pg;
+var cnv;
 
 
 function preload() {
   song1 = loadSound('assets/iGetAKickOutOfYou.mp3'); //song file
-  song2 = loadSound('assets/naima.mp3');
-  song3 = loadSound('assets/loveThemeFromSpartacus.mp3');
-  song4 = loadSound('assets/upperFourHundred.mp3'); //song file
-  song5 = loadSound('assets/theGirlFromIpanema.mp3');
-  song6 = loadSound('assets/imConfessin.mp3');
-  song7 = loadSound('assets/affirmation.mp3');
+  song2 = loadSound('assets/dearOldStockholm.mp3');
+  song3 = loadSound('assets/naima.mp3');
+  song4 = loadSound('assets/upperFourHundred.mp3');
+  song5 = loadSound('assets/imConfessin.mp3');
+  song6 = loadSound('assets/willowWeepForMe.mp3');
+  song7 = loadSound('assets/stardust.mp3');
+  song8 = loadSound('assets/twistedBlues.mp3');
+  song9 = loadSound('assets/iGetAlongWithoutYou.mp3');
+  song10 = loadSound('assets/loveThemeFromSpartacus.mp3');
   
   myfont = loadFont('assets/constan.ttf'); //font file
   
@@ -31,30 +35,32 @@ function preload() {
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  cnv = createCanvas(windowWidth, windowHeight, WEBGL);
   
-  col1 = color(242,133,0);
-  col2 = color(254,216,177);
-  col3 = color(204,204,255);
+  col1 = color(210,180,140);
+  col2 = color(0,128,128);
+  col3 = color(242,133,0);
   col4 = color(190,190,190);
-  col5 = color(254,216,177);
-  col6 = color(76,187,23);
-  col7 = color(204,204,255);
-  
+  col5 = color(76,187,23);
+  col6 = color(255,239,0);
+  col7 = color(128,0,32);
+  col8 = color(245,245,220);
+  col9 = color(181,126,220);
+  col10 = color(135,206,235);
 
   
   textFont(myfont);
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
   
-  library = [song1,song2,song3,song4,song5,song6,song7]; //dictates "library" as selection of all songs
-  librarycol = [col1,col2,col3,col4,col5,col6,col7];
+  library = [song1,song2,song3,song4,song5,song6,song7,song8,song9,song10]; //dictates "library" as selection of all songs
+  librarycol = [col1,col2,col3,col4,col5,col6,col7,col8,col9,col10];
 
-  which = random(0,7);
+  which = random(0,10);
   songs = library[int(which)];
   colour = librarycol[int(which)];
 
-  
+  pg = createGraphics(175,100);
   
   amp = new p5.Amplitude(); //amp is taking the amplitude of the song
   frameRate(100);
@@ -75,27 +81,25 @@ function draw() {
     
     push();
       time = millis();
-      textSize(75);
-      translate(0,-height/3);
+      textSize(150);
       rotateX(time / 1000);
       rotateZ(time / 1234);
       text('jazzcats', 0, 0);
     pop();
     
-    textSize(32);
-    text('a jazz influenced generative artwork',0,-150);
+    
     textSize(24);
-    text('controls',0,150);
+    text('a jazz influenced generative artwork',0,-height/3);
     textSize(18);
-    text('"T" = start sketch',0,200);
-    text('"N" = next song',0,225);
-    text('"S" = save sketch',0,250);
-    text('"R" = reset sketch',0,275);
-    text('"left mouse click" = draw',0,300);
+    text('press "T" to start',0,-height/4);
     
   }
   
   if (menu == 1) {
+    
+
+    
+    
     push();
       fill(0,20);
       stroke(colour);
@@ -110,7 +114,7 @@ function draw() {
       var v = volume*t*200;
       fill(colour);
       stroke(255);
-      ellipse(sin(v / 50) * (v / 5), cos(v / 50) * v / 5, sin(v / 30) * 100, 100);
+      ellipse(sin(v / 50) * (v / 5), cos(v / 50) * v / 5, sin(v / 30) * 75, 75);
     pop();
    
    if(mouseIsPressed){
@@ -119,10 +123,19 @@ function draw() {
     stroke(colour);
     r += 0.02;
     rotate(r);
-    ellipse(mouseX-width/2,mouseY-height/2,volume*1000,volume*1000);
+    ellipse(mouseX-width/2,mouseY-height/2,volume*750,volume*750);
     pop();
    }
-
+   push();
+    pg.background(0);
+    pg.textSize(12);
+    pg.fill(255);
+    pg.text('"N" = next song',pg.width/10,pg.height-15);
+    pg.text('"R" = reset',pg.width/10,pg.height-30);
+    pg.text('"S" = save sketch',pg.width/10,pg.height-45);
+    pg.text('"left mouse click" = draw',pg.width/10,pg.height-60);
+    image(pg,-width/2,-height/2);
+   pop();
 }
  
   
@@ -138,7 +151,7 @@ function keyTyped() {
   if (menu == 0 && key == 't') {
     background(0);
     loop();
-    which = random(0,7);
+    which = random(0,10);
     songs = library[int(which)];
     colour = librarycol[int(which)];
     amp.setInput(songs);
@@ -148,18 +161,19 @@ function keyTyped() {
     songs.stop();
     menu = 0;
   }else if(menu == 1 && key == 's'){
-    saveCanvas('myCanvas','png');
+    save(cnv,'myCanvas.jpg');
     songs.stop();
     noLoop();
     menu = 0;
+    loop();
   }else if(menu == 1 && key == 'n'){
     prevSong = songs;
     songs.stop();
-    which = random(0,7);
+    which = random(0,10);
     songs = library[int(which)];
     colour = librarycol[int(which)];
       if (prevSong == songs){
-        which = random(0,7);
+        which = random(0,10);
         songs = library[int(which)];
         colour = librarycol[int(which)];
         amp.setInput(songs);
